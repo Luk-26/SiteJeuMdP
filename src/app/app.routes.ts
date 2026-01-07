5d53df5e0c640b943d6deadc6f3cac1233617f3d
luk26 <lucas.guillemaud@gmail.com>
2026-01-07 11:15:48 +0100

    Création du header (navbar) et du footer ainsi que du .icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { Jeumdp } from './pages/jeumdp/jeumdp';
+import { Outils } from './pages/outils/outils';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Accueil },
+    { path: 'jeu-mdp', component: Jeumdp },
+    { path: 'boite-a-outils', component: Outils },
+];

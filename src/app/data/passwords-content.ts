d9304566629f59c995ace07156d17622559b8a81
luk26 <lucas.guillemaud@gmail.com>
2026-01-08 16:53:22 +0100

    Ajouts d'éléments externe

---
+++ src/app/data/passwords-content.ts
@@ -0,0 +1,101 @@
+export const CSV_CONTENT = `ID,Mot de passe (exemple),Longueur,Catégorie (table Hive),Temps max estimé de craquage,Niveau (couleur)
+1,33890838637940,14,Chiffres uniquement,28 ans,Orange
+2,26542,5,Chiffres uniquement,Instantané,Violet
+3,3511,4,Chiffres uniquement,Instantané,Violet
+4,615594078161849,15,Chiffres uniquement,284 ans,Orange
+5,59310341,8,Chiffres uniquement,Instantané,Violet
+6,3164752,7,Chiffres uniquement,Instantané,Violet
+7,5534192,7,Chiffres uniquement,Instantané,Violet
+8,832764,6,Chiffres uniquement,Instantané,Violet
+9,835030564139537,15,Chiffres uniquement,284 ans,Orange
+10,67242,5,Chiffres uniquement,Instantané,Violet
+11,38849696532871,14,Chiffres uniquement,28 ans,Orange
+12,012269166978480,15,Chiffres uniquement,284 ans,Orange
+13,184514627048281489,18,Chiffres uniquement,284 mille ans,Jaune
+14,325288095701,12,Chiffres uniquement,3 mois,Orange
+15,54303,5,Chiffres uniquement,Instantané,Violet
+16,9117182278248,13,Chiffres uniquement,3 ans,Orange
+17,9638346578,10,Chiffres uniquement,1 jour,Rouge
+18,7133,4,Chiffres uniquement,Instantané,Violet
+19,1509,4,Chiffres uniquement,Instantané,Violet
+20,83930,5,Chiffres uniquement,Instantané,Violet
+21,ssphz,5,Lettres minuscules,Instantané,Violet
+22,pzngddvnlnnoxbv,15,Lettres minuscules,477 Millions d'années,Vert
+23,uudbmxkzdhggro,14,Lettres minuscules,18 Millions d'années,Vert
+24,enfi,4,Lettres minuscules,Instantané,Violet
+25,ohcozrd,7,Lettres minuscules,20 heures,Rouge
+26,burac,5,Lettres minuscules,Instantané,Violet
+27,yhfnppgmbfmamizzoj,18,Lettres minuscules,8 Billions d'années,Vert
+28,nwxz,4,Lettres minuscules,Instantané,Violet
+29,rvwpegjgbsxrbxkbb,17,Lettres minuscules,322 Milliards d'années,Vert
+30,spqqfbqcf,9,Lettres minuscules,2 ans,Orange
+31,ctcvh,5,Lettres minuscules,Instantané,Violet
+32,mdshstbtcnvs,12,Lettres minuscules,27 mille ans,Jaune
+33,sqkigvw,7,Lettres minuscules,20 heures,Rouge
+34,khimevuj,8,Lettres minuscules,3 semaines,Rouge
+35,okycaotsdcrgqi,14,Lettres minuscules,18 Millions d'années,Vert
+36,elchljforwj,11,Lettres minuscules,1 mille ans,Jaune
+37,tzuqavr,7,Lettres minuscules,20 heures,Rouge
+38,jvdeiddxreij,12,Lettres minuscules,27 mille ans,Jaune
+39,tgwkgv,6,Lettres minuscules,46 minutes,Rouge
+40,uiqpibcunibakye,15,Lettres minuscules,477 Millions d'années,Vert
+41,ipsxnRcBjtYcRw,14,Lettres majuscules et minuscules,300 Milliards d'années,Vert
+42,VQZZxKYA,8,Lettres majuscules et minuscules,15 ans,Orange
+43,twDhYG,6,Lettres majuscules et minuscules,2 jours,Rouge
+44,GIRePrvwKmbzrXh,15,Lettres majuscules et minuscules,15 Billions d'années,Vert
+45,SaZcmHmBTqI,11,Lettres majuscules et minuscules,2 Millions d'années,Vert
+46,sAJSxziQGJut,12,Lettres majuscules et minuscules,111 Millions d'années,Vert
+47,MEnRYCDYuCPZnLG,15,Lettres majuscules et minuscules,15 Billions d'années,Vert
+48,XapNBDAeEO,10,Lettres majuscules et minuscules,41 mille ans,Jaune
+49,yoHdBDlgZYXS,12,Lettres majuscules et minuscules,111 Millions d'années,Vert
+50,rkOV,4,Lettres majuscules et minuscules,Instantané,Violet
+51,HyOrX,5,Lettres majuscules et minuscules,57 minutes,Rouge
+52,iTseO,5,Lettres majuscules et minuscules,57 minutes,Rouge
+53,oTCjyiejdArAnDIAvS,18,Lettres majuscules et minuscules,2 Trillions d'années,Vert
+54,FkzUvBbHZypQRrD,15,Lettres majuscules et minuscules,15 Billions d'années,Vert
+55,buufDrvynViEqIWWAM,18,Lettres majuscules et minuscules,2 Trillions d'années,Vert
+56,VKpnhD,6,Lettres majuscules et minuscules,2 jours,Rouge
+57,KRtyLtbzTyFg,12,Lettres majuscules et minuscules,111 Millions d'années,Vert
+58,QRlB,4,Lettres majuscules et minuscules,Instantané,Violet
+59,aBNUDTFNgxBjlHOPr,17,Lettres majuscules et minuscules,42 Billiards d'années,Vert
+60,AuwknZFFl,9,Lettres majuscules et minuscules,791 ans,Orange
+61,wHLQ4,5,"Chiffres, majuscules et minuscules",2 heures,Rouge
+62,vwnXSD8,7,"Chiffres, majuscules et minuscules",1 an,Jaune
+63,mLWlk8rSEU8DWVn,15,"Chiffres, majuscules et minuscules",218 Billions d'années,Vert
+64,F3W4iOS8bR,10,"Chiffres, majuscules et minuscules",238 mille ans,Jaune
+65,zeK0FPReO1i,11,"Chiffres, majuscules et minuscules",14 Millions d'années,Vert
+66,t63t6LCCQKyB,12,"Chiffres, majuscules et minuscules",917 Millions d'années,Vert
+67,1SMAscfCoEsaksRT,16,"Chiffres, majuscules et minuscules",13 Billiards d'années,Vert
+68,MBcY2tV,7,"Chiffres, majuscules et minuscules",1 an,Jaune
+69,VI6BMCF4c8JwrfG,15,"Chiffres, majuscules et minuscules",218 Billions d'années,Vert
+70,iEOFwBf9FA0,11,"Chiffres, majuscules et minuscules",14 Millions d'années,Vert
+71,3RuGX0fD0quhzy,14,"Chiffres, majuscules et minuscules",3 Billions d'années,Vert
+72,Si7BsbOyCJ3Frh4,15,"Chiffres, majuscules et minuscules",218 Billions d'années,Vert
+73,sd2TTU7rFAG,11,"Chiffres, majuscules et minuscules",14 Millions d'années,Vert
+74,cYI0Gsv6ra7,11,"Chiffres, majuscules et minuscules",14 Millions d'années,Vert
+75,KSK2XEpcoo3VTAyu,16,"Chiffres, majuscules et minuscules",13 Billiards d'années,Vert
+76,0qUO,4,"Chiffres, majuscules et minuscules",Instantané,Violet
+77,N4Pcv,5,"Chiffres, majuscules et minuscules",2 heures,Rouge
+78,ekIU7Nc3,8,"Chiffres, majuscules et minuscules",62 ans,Orange
+79,CkDS1tn,7,"Chiffres, majuscules et minuscules",1 an,Jaune
+80,YlARKpHNl2,10,"Chiffres, majuscules et minuscules",238 mille ans,Jaune
+81,66MUjz!,7,"Chiffres, majuscules, minuscules et symboles",2 ans,Orange
+82,bgDsJ4Y*^JK,11,"Chiffres, majuscules, minuscules et symboles",56 Millions d'années,Vert
+83,z^Zj#1lLw7UqPq,14,"Chiffres, majuscules, minuscules et symboles",19 Billions d'années,Vert
+84,lYY3@ebU,8,"Chiffres, majuscules, minuscules et symboles",164 ans,Orange
+85,C78*Pis5MdD,11,"Chiffres, majuscules, minuscules et symboles",56 Millions d'années,Vert
+86,6R1#hV@2,8,"Chiffres, majuscules, minuscules et symboles",164 ans,Orange
+87,mE4YV5#ihTUB$4,14,"Chiffres, majuscules, minuscules et symboles",19 Billions d'années,Vert
+88,8$cY,4,"Chiffres, majuscules, minuscules et symboles",Instantané,Violet
+89,c^wyJTqb#oEPqd9EGs,18,"Chiffres, majuscules, minuscules et symboles",463 Trillions d'années,Vert
+90,thkj94!dZ^n2P8B6,16,"Chiffres, majuscules, minuscules et symboles",94 Billiards d'années,Vert
+91,$6d30R1Z5ZD,11,"Chiffres, majuscules, minuscules et symboles",56 Millions d'années,Vert
+92,TK023gcQhmCTqp1VN*,18,"Chiffres, majuscules, minuscules et symboles",463 Trillions d'années,Vert
+93,x4N^RVtI,8,"Chiffres, majuscules, minuscules et symboles",164 ans,Orange
+94,*1uBV&W60xUPRN,14,"Chiffres, majuscules, minuscules et symboles",19 Billions d'années,Vert
+95,I7oB#Nvo$S31,12,"Chiffres, majuscules, minuscules et symboles",3 Milliards d'années,Vert
+96,LG2!Pj,6,"Chiffres, majuscules, minuscules et symboles",2 semaines,Rouge
+97,$Ef6%,5,"Chiffres, majuscules, minuscules et symboles",4 heures,Rouge
+98,21Z6^6LjxOB,11,"Chiffres, majuscules, minuscules et symboles",56 Millions d'années,Vert
+99,NR6&dHYk@,9,"Chiffres, majuscules, minuscules et symboles",11 mille ans,Jaune
+100,Ey%L5zsnwjGtw,13,"Chiffres, majuscules, minuscules et symboles",275 Milliards d'années,Vert`;

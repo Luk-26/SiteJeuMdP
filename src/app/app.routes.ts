a40fc17d7f09bee07128f8a8e55e2d1f09885d72
luk26 <lucas.guillemaud@gmail.com>
2026-01-21 11:59:08 +0100

    Modif title pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { Outils } from './pages/outils/outils';
 // Définition des routes de l'application.
 // Associe des chemins d'URL à des composants spécifiques.
 export const routes: Routes = [
-    { path: '', component: Accueil }, // Page d'accueil (racine)
-    { path: 'jeu-mdp', component: Jeumdp }, // Jeu interactif
-    { path: 'boite-a-outils', component: Outils }, // Outils (Générateur/Testeur)
+    { path: '', component: Accueil, title: 'Accueil' }, // Page d'accueil (racine)
+    { path: 'jeu-mdp', component: Jeumdp, title: 'Jeu maux de passe' }, // Jeu interactif
+    { path: 'boite-a-outils', component: Outils, title: 'Boite à outils' }, // Outils (Générateur/Testeur)
 ];
